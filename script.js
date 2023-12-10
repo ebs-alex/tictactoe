4a30004ef469b013a47746f68014ef88774d6618
Alex Moran <ebs.alex@icloud.com>
2023-12-10 13:19:38 -0800

    began board and game engine modules and began interfacing with each other. board module now registers turn input into tictactoe cells.

--- script.js
+++ script.js
@@ -1,9 +1,62 @@
 
 
-let cells = document.querySelectorAll(".cell");
-cells.forEach((cell) => cell.addEventListener("click", () => cellclicked(cell.id)))
 
 
-function cellclicked(cellID) {
-    alert(cellID)
-}
+
+const gameEngine = (function(){
+
+    let x = ''
+
+    function registerTurn(turn, cell) {
+        x = turn;
+        console.log(turn);
+        console.log(cell.id);
+    }
+
+
+
+    return {registerTurn}
+
+})();
+
+
+const board = (function(){
+    let cells = document.querySelectorAll(".cell");
+    cells.forEach((cell) => cell.addEventListener("click", () => cellClicked(cell)));
+    let turnSpan = document.querySelector("#turnSpan");
+    let turn = "X";
+    turnSpan.textContent = turn;
+
+    function cellClicked(cell) {
+        if (cellUnoccupied(cell)) {
+            markCell(cell);
+            changeTurn()
+        } else {
+            alert("that cell is already occupied!")
+        }
+    }
+
+    function markCell(cell) {
+        cell.textContent = turn;
+        gameEngine.registerTurn(turn, cell)
+        console.log(gameEngine.x)
+    }
+
+    function changeTurn() {
+        if (turn == "X") {
+            turn = "O"
+        } else {
+            turn = "X"
+        }
+        turnSpan.textContent = turn;
+    }
+
+    function cellUnoccupied(cell) {
+        if (cell.textContent) {
+            return false
+        } else {
+            return true
+        }
+    }
+})();
+
